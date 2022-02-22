39ff510d45e7d8d60f7fccaf7eb3e6422fc480e2
mark-summerfield <mark@qtrac.eu>
2022-02-22 09:50:56 +0000

    more progress on new_list_form

--- src/list_actions.rs
+++ src/list_actions.rs
@@ -2,13 +2,37 @@
 // License: GPLv3
 
 use crate::application::Application;
-use crate::new_list_form::{self, NewListResult};
+use crate::new_list_form;
 
 impl Application {
     pub(crate) fn on_list_new(&mut self) {
-        let form = new_list_form::Form::default();
-        let reply = &*form.result.borrow();
-        println!("on_list_new: {:?}", reply); // TODO
+        let mut top_levels = vec![];
+        if let Some(root) = self.tlm.track_tree.first() {
+            let mut opt_item = root.next(); // first top-level child
+            while let Some(item) = opt_item {
+                opt_item = item.next_sibling();
+                if let Some(name) = item.label() {
+                    top_levels.push(name);
+                }
+            }
+        }
+        let form =
+            new_list_form::Form::new(&self.current_track, &top_levels);
+        if *form.ok.borrow() {
+            let name = &*form.name.borrow();
+            let parent_list = &*form.parent_list.borrow();
+            let folder_or_playlist = &*form.folder_or_playlist.borrow();
+            let include_subdirs = *form.include_subdirs.borrow();
+            dbg!(
+                "on_list_new: OK",
+                name,
+                parent_list,
+                folder_or_playlist,
+                include_subdirs
+            ); // TODO
+        } else {
+            dbg!("on_list_new: Cancel"); // TODO DELETE
+        }
     }
 
     pub(crate) fn on_list_rename(&mut self) {
--- src/new_list_form.rs
+++ src/new_list_form.rs
@@ -1,63 +1,53 @@
 // Copyright © 2022 Mark Summerfield. All rights reserved.
 // License: GPLv3
 
-use super::CONFIG;
-use crate::fixed::{
-    APPNAME, BUTTON_HEIGHT, BUTTON_WIDTH, ICON, MAX_HISTORY_SIZE,
-    MIN_HISTORY_SIZE, PAD, SCALE_MAX, SCALE_MIN,
-};
+use crate::fixed::{APPNAME, BUTTON_HEIGHT, BUTTON_WIDTH, ICON, PAD};
 use crate::util;
 use fltk::{
     app,
     button::{Button, CheckButton, RadioRoundButton},
-    enums::{Align, FrameType},
+    dialog::{FileDialog, FileDialogType},
+    enums::FrameType,
     frame::Frame,
     group::Flex,
     image::SvgImage,
     input::Input,
     menu::Choice,
-    misc::Spinner,
     prelude::*,
     window::Window,
 };
-use std::{cell::RefCell, path::PathBuf, rc::Rc};
+use std::{
+    cell::RefCell,
+    path::{Path, PathBuf},
+    rc::Rc,
+};
 
 // if folder_or_playlist is a folder then read tracks from the folder;
 // if folder_or_playlist is a file then read tracks from the playlist file
-#[derive(Clone, Debug)]
-pub struct NewListResult {
-    pub ok: bool, // false means user canceled; true means create new list
-    pub name: Option<String>, // None means use stem of folder_or_playlist
-    pub parent_list: Option<String>, // None means top-level
-    pub folder_or_playlist: Option<PathBuf>, // None means empty list
-    pub include_subdirs: bool, // only meaningful for folders
-}
-
-impl NewListResult {
-    pub fn default() -> Self {
-        NewListResult {
-            ok: false,
-            name: None,
-            parent_list: None,
-            folder_or_playlist: None,
-            include_subdirs: true,
-        }
-    }
-}
-
+// empty name means use stem of folder_or_playlist
+// empty parent_list means top-level
+// empty folder_or_playlist means empty list
 pub struct Form {
     form: Window,
-    pub result: Rc<RefCell<NewListResult>>,
+    pub ok: Rc<RefCell<bool>>,
+    pub name: Rc<RefCell<String>>,
+    pub parent_list: Rc<RefCell<String>>,
+    pub folder_or_playlist: Rc<RefCell<PathBuf>>,
+    pub include_subdirs: Rc<RefCell<bool>>,
 }
 
-impl Default for Form {
-    fn default() -> Self {
-        let result = Rc::from(RefCell::from(NewListResult::default()));
+impl Form {
+    pub fn new(current_track: &Path, top_levels: &[String]) -> Self {
+        let ok = Rc::from(RefCell::from(false));
+        let name = Rc::from(RefCell::from(String::new()));
+        let parent_list = Rc::from(RefCell::from(String::new()));
+        let folder_or_playlist = Rc::from(RefCell::from(PathBuf::new()));
+        let include_subdirs = Rc::from(RefCell::from(false));
         let mut form = make_form();
         let mut vbox = Flex::default().size_of_parent().column();
         vbox.set_margin(PAD);
         vbox.set_pad(PAD);
-        let mut widgets = make_widgets();
+        let mut widgets = make_widgets(top_levels);
         let (button_row, mut buttons) = make_buttons();
         vbox.set_size(&button_row, BUTTON_HEIGHT);
         vbox.end();
@@ -65,16 +55,28 @@ impl Default for Form {
         form.make_modal(true);
         add_event_handlers(
             &mut form,
-            &widgets,
+            &mut widgets,
             &mut buttons,
-            Rc::clone(&result),
+            current_track.to_path_buf(),
+            Rc::clone(&ok),
+            Rc::clone(&name),
+            Rc::clone(&parent_list),
+            Rc::clone(&folder_or_playlist),
+            Rc::clone(&include_subdirs),
         );
         widgets.parent_list_combo.take_focus().unwrap();
         form.show();
         while form.shown() {
             app::wait();
         }
-        Self { form, result }
+        Self {
+            form,
+            ok,
+            name,
+            parent_list,
+            folder_or_playlist,
+            include_subdirs,
+        }
     }
 }
 
@@ -85,9 +87,14 @@ impl Drop for Form {
 }
 
 struct Widgets {
+    pub parent_list_button: Button,
     pub parent_list_combo: Choice,
+    pub folder_radio: RadioRoundButton,
+    pub playlist_radio: RadioRoundButton,
+    pub choose_button: Button,
     pub folder_or_playlist_label: Frame,
     pub include_subdirs_checkbox: CheckButton,
+    pub name_button: Button,
     pub name_input: Input,
 }
 
@@ -108,9 +115,8 @@ fn make_form() -> Window {
     form
 }
 
-fn make_widgets() -> Widgets {
-    let left_width = (WIDTH / 5).max(BUTTON_WIDTH * 2);
-    let right_width = (WIDTH / 4).max(BUTTON_WIDTH * 2);
+fn make_widgets(top_levels: &[String]) -> Widgets {
+    let width = (WIDTH / 5).max(BUTTON_WIDTH * 2);
     let mut column = Flex::default().column();
     column.set_pad(PAD);
     let mut row1 = Flex::default().row();
@@ -119,53 +125,60 @@ fn make_widgets() -> Widgets {
         Button::default().with_label("&Parent List");
     parent_list_button.set_frame(FrameType::NoBox);
     parent_list_button.visible_focus(false);
-    // TODO add handler to make it give focus to parent_list_combo
     let mut parent_list_combo = Choice::default();
     parent_list_combo.add_choice("<Top-Level>");
+    for top_level in top_levels {
+        parent_list_combo.add_choice(&top_level);
+    }
     parent_list_combo.set_value(0);
-    // TODO add other top-level lists
-    row1.set_size(&parent_list_button, left_width);
+    row1.set_size(&parent_list_button, width);
     row1.end();
     let mut row2 = Flex::default().row();
     row2.set_pad(PAD);
-    // TODO these aren't showing as radio buttons!
-    let folder_radio =
+    let mut folder_radio =
         RadioRoundButton::default().with_label("&Add tracks from folder");
+    folder_radio.toggle(true);
     let playlist_radio = RadioRoundButton::default()
         .with_label("&Import tracks from playist");
-    let mut empty_radio =
-        RadioRoundButton::default().with_label("Create &Empty list");
-    empty_radio.toggle(true);
+    RadioRoundButton::default().with_label("Create &Empty list");
     row2.end();
     let mut row3 = Flex::default().row();
     row3.set_pad(PAD);
     let choose_button = Button::default().with_label("C&hoose…");
-    let mut folder_or_playlist_label = Frame::default(); // TODO
+    let mut folder_or_playlist_label = Frame::default();
     folder_or_playlist_label.set_frame(FrameType::DownFrame);
-    let mut include_subdirs_checkbox =
-        CheckButton::default().with_label("Include &Subfolders"); // TODO
-    include_subdirs_checkbox.set_checked(true);
-    row3.set_size(&choose_button, left_width);
-    row3.set_size(&include_subdirs_checkbox, right_width);
+    row3.set_size(&choose_button, width);
     row3.end();
     let mut row4 = Flex::default().row();
     row4.set_pad(PAD);
+    let include_subdirs_checkbox =
+        CheckButton::default().with_label("Include &Subfolders");
+    include_subdirs_checkbox.set_checked(true);
+    row4.set_size(&include_subdirs_checkbox, width);
+    row4.end();
+    let mut row5 = Flex::default().row();
+    row5.set_pad(PAD);
     let mut name_button = Button::default().with_label("&Name");
     name_button.set_frame(FrameType::NoBox);
     name_button.visible_focus(false);
-    // TODO add handler to make it give focus to name_input
     let name_input = Input::default();
-    row4.set_size(&name_button, left_width);
-    row4.end();
+    row5.set_size(&name_button, width);
+    row5.end();
     column.end();
     column.set_size(&row1, BUTTON_HEIGHT);
     column.set_size(&row2, BUTTON_HEIGHT);
     column.set_size(&row3, BUTTON_HEIGHT);
     column.set_size(&row4, BUTTON_HEIGHT);
+    column.set_size(&row5, BUTTON_HEIGHT);
     Widgets {
+        parent_list_button,
         parent_list_combo,
+        folder_radio,
+        playlist_radio,
+        choose_button,
         folder_or_playlist_label,
         include_subdirs_checkbox,
+        name_button,
         name_input,
     }
 }
@@ -185,16 +198,53 @@ fn make_buttons() -> (Flex, Buttons) {
 
 fn add_event_handlers(
     form: &mut Window,
-    widgets: &Widgets,
+    widgets: &mut Widgets,
     buttons: &mut Buttons,
-    result: Rc<RefCell<NewListResult>>,
+    current_track: PathBuf,
+    ok: Rc<RefCell<bool>>,
+    name: Rc<RefCell<String>>,
+    parent_list: Rc<RefCell<String>>,
+    folder_or_playlist: Rc<RefCell<PathBuf>>,
+    include_subdirs: Rc<RefCell<bool>>,
 ) {
-    buttons.ok_button.set_callback({
-        // TODO
-        let mut form = form.clone();
+    widgets.parent_list_button.set_callback({
+        let mut parent_list_combo = widgets.parent_list_combo.clone();
         move |_| {
-            let result = &*result.borrow_mut();
+            parent_list_combo.take_focus().unwrap();
+        }
+    });
+    widgets.name_button.set_callback({
+        let mut name_input = widgets.name_input.clone();
+        move |_| {
+            name_input.take_focus().unwrap();
+        }
+    });
+    widgets.choose_button.set_callback({
+        let folder_radio = widgets.folder_radio.clone();
+        let playlist_radio = widgets.playlist_radio.clone();
+        let mut folder_or_playlist_label =
+            widgets.folder_or_playlist_label.clone();
+        move |_| {
+            if folder_radio.is_toggled() {
+                folder_or_playlist_label
+                    .set_label(&get_tracks_folder(current_track.clone()));
+            } else if playlist_radio.is_toggled() {
+                folder_or_playlist_label.set_label(&get_playlist_filename(
+                    current_track.clone(),
+                ));
+            }
+            app::redraw(); // redraws the world
+        }
+    });
+    buttons.ok_button.set_callback({
+        let mut form = form.clone();
+        let mut name = name.clone();
+        let name_input = widgets.name_input.clone();
+        move |_| {
             // TODO
+            *ok.borrow_mut() = true;
+            dbg!(&name_input.label());
+            *name.borrow_mut() = name_input.label();
             form.hide();
         }
     });
@@ -206,5 +256,32 @@ fn add_event_handlers(
     });
 }
 
+fn get_tracks_folder(current_track: PathBuf) -> String {
+    let mut form = FileDialog::new(FileDialogType::BrowseDir);
+    form.set_title(&format!("Read Folder — {APPNAME}"));
+    let _ = form.set_directory(&util::get_track_dir(&current_track));
+    form.show();
+    let folder = form.filename();
+    if folder.exists() {
+        folder.display().to_string()
+    } else {
+        String::new()
+    }
+}
+
+fn get_playlist_filename(current_track: PathBuf) -> String {
+    let mut form = FileDialog::new(FileDialogType::BrowseFile);
+    form.set_title(&format!("Read Playlist — {APPNAME}"));
+    let _ = form.set_directory(&util::get_track_dir(&current_track));
+    form.set_filter("Playlists\t*.{m3u,pls,xspf}");
+    form.show();
+    let filename = form.filename();
+    if filename.exists() {
+        filename.display().to_string()
+    } else {
+        String::new()
+    }
+}
+
 const WIDTH: i32 = 640;
-const HEIGHT: i32 = PAD + ((BUTTON_HEIGHT + PAD) * 5);
+const HEIGHT: i32 = PAD + ((BUTTON_HEIGHT + PAD) * 6);
