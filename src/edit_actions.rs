f5578501c2d53631a7d698909efad89d7311530e
mark-summerfield <mark@qtrac.eu>
2022-03-08 09:37:10 +0000

    tiny

--- src/edit_actions.rs
+++ src/edit_actions.rs
@@ -4,20 +4,20 @@
 use crate::application::Application;
 
 impl Application {
-    pub(crate) fn on_edit_promote(&mut self) {
-        println!("on_edit_promote"); // TODO
-    }
-
-    pub(crate) fn on_edit_demote(&mut self) {
-        println!("on_edit_demote"); // TODO
-    }
-
     pub(crate) fn on_edit_move_up(&mut self) {
         println!("on_edit_move_up"); // TODO
     }
 
     pub(crate) fn on_edit_move_down(&mut self) {
         println!("on_edit_move_down"); // TODO
+    }
+
+    pub(crate) fn on_edit_promote(&mut self) {
+        println!("on_edit_promote"); // TODO
+    }
+
+    pub(crate) fn on_edit_demote(&mut self) {
+        println!("on_edit_demote"); // TODO
     }
 
     pub(crate) fn on_edit_move_to_list(&mut self) {
--- st.sh
+++ st.sh
@@ -5,6 +5,6 @@ python3 -m flake8 --ignore=E261,E303,W504 *.py
 python3 -m vulture *.py \
     | grep -v '60% confidenc'
 cd ..
-tokei -f -slines -c80 -tPython,Rust -etarget
+tokei -f -slines -c80 -tRust -etarget
 grep --color=auto --exclude-dir=target --include=*.rs -r format......,
 git st
