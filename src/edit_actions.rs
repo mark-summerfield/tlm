257dfed16d49e784f57a814066ef8bb5623824ab
mark-summerfield <mark@qtrac.eu>
2022-03-09 10:39:44 +0000

    can now move tracks up and down within their list

--- src/edit_actions.rs
+++ src/edit_actions.rs
@@ -2,18 +2,58 @@
 // License: GPLv3
 
 use crate::application::Application;
+use fltk::app;
 
 impl Application {
     pub(crate) fn on_edit_move_up(&mut self) {
-        println!("on_edit_move_up"); // TODO
+        if let Some(mut item) = self.tlm.track_tree.first_selected_item() {
+            if let Some(prev) = item.prev_sibling() {
+                if item.move_above(&prev).is_ok() {
+                    app::redraw(); // redraws the world
+                }
+            }
+        }
     }
 
     pub(crate) fn on_edit_move_down(&mut self) {
-        println!("on_edit_move_down"); // TODO
+        if let Some(mut item) = self.tlm.track_tree.first_selected_item() {
+            if let Some(next) = item.next_sibling() {
+                if item.move_below(&next).is_ok() {
+                    app::redraw(); // redraws the world
+                }
+            }
+        }
     }
 
     pub(crate) fn on_edit_promote(&mut self) {
         println!("on_edit_promote"); // TODO
+
+        /*
+            if let Some(item) = self.tlm.track_tree.first_selected_item() {
+                if let Some(parent) = item.parent() {
+                    if parent.is_root() {
+                        return; // can't promote beyond the root
+                    }
+                    if let Some(mut grand_parent) = parent.parent() {
+                        let index = grand_parent.children(); // pos
+                        ///
+                        let err = grand_parent.reparent(&item, index);
+                        dbg!(
+                            item.label(),
+                            parent.label(),
+                            grand_parent.label(),
+                            index,
+                            err
+                        );
+                        app::redraw(); // redraws the world
+
+                        // if grand_parent.reparent(&item, index).is_ok() {
+                        //     app::redraw(); // redraws the world
+                        // }
+                    }
+                }
+            }
+        */
     }
 
     pub(crate) fn on_edit_demote(&mut self) {
