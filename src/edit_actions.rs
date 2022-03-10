a93d5e3b06abcdced7cddca487005c3622d9f18e
mark-summerfield <mark@qtrac.eu>
2022-03-10 08:21:26 +0000

    stuck on promote/demote not working (maybe fltk-rs bug? have reported)

--- src/edit_actions.rs
+++ src/edit_actions.rs
@@ -25,37 +25,31 @@ impl Application {
         }
     }
 
+    /*
+    Move the selected item to be the last child of its grandparent if it has
+    one. (Top-level items have a parent of root and no grandparent, so can't
+    be promoted.)
+    */
     pub(crate) fn on_edit_promote(&mut self) {
-        println!("on_edit_promote"); // TODO
-
-        /*
-            if let Some(item) = self.tlm.track_tree.first_selected_item() {
-                if let Some(parent) = item.parent() {
-                    if parent.is_root() {
-                        return; // can't promote beyond the root
-                    }
-                    if let Some(mut grand_parent) = parent.parent() {
-                        let index = grand_parent.children(); // pos
-                        ///
-                        let err = grand_parent.reparent(&item, index);
-                        dbg!(
-                            item.label(),
-                            parent.label(),
-                            grand_parent.label(),
-                            index,
-                            err
-                        );
-                        app::redraw(); // redraws the world
-
-                        // if grand_parent.reparent(&item, index).is_ok() {
-                        //     app::redraw(); // redraws the world
-                        // }
-                    }
+        if let Some(item) = self.tlm.track_tree.first_selected_item() {
+            if let Some(parent) = item.parent() {
+                if parent.is_root() {
+                    return; // can't promote beyond the root
+                }
+                if let Some(mut grand_parent) = parent.parent() {
+                    let index = grand_parent.children() + 1;
+                    let err = grand_parent.move_into(&item, index);
+                    // let err = grand_parent.move_into(&item, index);
+                    dbg!(item.label(), parent.label(), grand_parent.label(), index, err);
                 }
             }
-        */
+        }
     }
 
+    /*
+    Move the selected item to be the last child of the first _following_
+    sibling that is a _list_ (i.e., that has no TID) if there is one.
+    */
     pub(crate) fn on_edit_demote(&mut self) {
         println!("on_edit_demote"); // TODO
     }
